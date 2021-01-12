b53e5ff5092c002546e3e650faa4250b2f723c25
Deyvison <deyvisonjp@id.uff.br>
2021-01-12 18:58:18 -0300

    Configrando editor e eslint

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -3,11 +3,11 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Primeiro Projeto Node' })
+  return response.json({ message: 'Primeiro Projeto Node' });
 });
 
 app.listen(3333, () => {
-  console.log('🛩 Server started on port 3333 🛩')
+  console.log('✈ Server started on port 3333 ✈');
 })
 
 
